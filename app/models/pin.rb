7a65a24898dc8dd42b73b49835e6504a22271a32
trickertite <trickertite@gmail.com>
2015-04-05 22:59:17 +0300

    Image uploading working with paperclip gem

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,5 @@
 class Pin < ActiveRecord::Base
+	has_attached_file :image, styles: { medium: '300x300>'}
+	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 	belongs_to :user
 end
